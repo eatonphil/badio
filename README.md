720c3ee0482e6dcb1dd49d1789bccf86747b7776
Phil Eaton <phil@eatonphil.com>
2023-09-30 17:27:09 +0000

    Add example

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
+# badio
+
+ptrace layer for triggering bad IO behavior.
+
+Example:
+
+```
+$ make
+$ (cd test && go build main.go)
+$ cat test/main.go
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	f, err := os.OpenFile("test.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	text := "some great stuff"
+	_, _ = f.Write([]byte(text))
+
+	_ = f.Close()
+}
+$ ./main test/main
+Got a write on 3: some great stu
+$ cat test.txt
+some great stu
+```
+
 ## Notes
 
 * https://nullprogram.com/blog/2018/06/23/
--- main.zig
+++ main.zig
@@ -6,6 +6,169 @@ const c = @cImport({
     @cInclude("errno.h");
 });
 
+const cNullPtr: ?*anyopaque = null;
+
+fn execChild(
+    arena: *std.heap.ArenaAllocator,
+    args: []const []const u8,
+    childPid: std.os.pid_t,
+) std.process.ExecvError {
+    _ = c.ptrace(c.PTRACE_TRACEME, childPid, cNullPtr, cNullPtr);
+    return std.process.execv(
+        arena.allocator(),
+        args[1..],
+    );
+}
+
+const ABIArguments = struct {
+    regs: c.user_regs_struct,
+
+    fn nth(aa: ABIArguments, i: u8) c_ulonglong {
+        std.debug.assert(i < 4);
+
+        return switch (i) {
+            0 => aa.regs.rdi,
+            1 => aa.regs.rsi,
+            2 => aa.regs.rdx,
+            else => unreachable,
+        };
+    }
+
+    fn setNth(aa: *ABIArguments, i: u8, value: c_ulonglong) void {
+        std.debug.assert(i < 4);
+
+        switch (i) {
+            0 => {
+                aa.regs.rdi = value;
+            },
+            1 => {
+                aa.regs.rsi = value;
+            },
+            2 => {
+                aa.regs.rdx = value;
+            },
+            else => unreachable,
+        }
+    }
+
+    fn result(aa: ABIArguments) c_ulonglong {
+        return aa.regs.rax;
+    }
+
+    fn setResult(aa: *ABIArguments, value: c_ulonglong) void {
+        aa.regs.rax = value;
+    }
+
+    fn function(aa: ABIArguments) c_ulonglong {
+        return aa.regs.orig_rax;
+    }
+};
+
+const ChildManager = struct {
+    arena: *std.heap.ArenaAllocator,
+    childPid: std.os.pid_t,
+
+    fn getABIArguments(cm: ChildManager) ABIArguments {
+        var args = ABIArguments{ .regs = undefined };
+        _ = c.ptrace(c.PTRACE_GETREGS, cm.childPid, cNullPtr, &args.regs);
+        return args;
+    }
+
+    fn setABIArguments(cm: ChildManager, args: *ABIArguments) void {
+        _ = c.ptrace(c.PTRACE_SETREGS, cm.childPid, cNullPtr, &args.regs);
+    }
+
+    fn childReadData(
+        cm: ChildManager,
+        address: c_ulonglong,
+        length: c_ulonglong,
+    ) !std.ArrayList(u8) {
+        var data = std.ArrayList(u8).init(cm.arena.allocator());
+        while (data.items.len < length) {
+            var word = c.ptrace(
+                c.PTRACE_PEEKDATA,
+                cm.childPid,
+                address + data.items.len,
+                cNullPtr,
+            );
+
+            for (std.mem.asBytes(&word)) |byte| {
+                if (data.items.len == length) {
+                    break;
+                }
+                try data.append(byte);
+            }
+        }
+        return data;
+    }
+
+    fn childWaitForSyscall(cm: ChildManager) i32 {
+        var status: i32 = 0;
+        _ = c.ptrace(c.PTRACE_SYSCALL, cm.childPid, cNullPtr, cNullPtr);
+        _ = c.waitpid(cm.childPid, &status, 0);
+        return status;
+    }
+
+    const hooks = &[_]struct {
+        syscall: c_ulonglong,
+        hook: *const fn (ChildManager, *ABIArguments) anyerror!void,
+    }{.{
+        .syscall = @intFromEnum(std.os.linux.syscalls.X64.write),
+        .hook = writeHandler,
+    }};
+
+    fn childInterceptSyscalls(
+        cm: *ChildManager,
+    ) !void {
+        while (true) {
+            // Handle syscall entrance
+            const status = cm.childWaitForSyscall();
+            if (status == 0) {
+                break;
+            }
+
+            var args: ABIArguments = cm.getABIArguments();
+            const address = args.function();
+
+            for (hooks) |hook| {
+                if (address == hook.syscall) {
+                    try hook.hook(cm.*, &args);
+                }
+            }
+        }
+    }
+
+    fn writeHandler(cm: ChildManager, entryArgs: *ABIArguments) anyerror!void {
+        const fd = entryArgs.nth(0);
+        const dataAddress = entryArgs.nth(1);
+        var dataLength = entryArgs.nth(2);
+
+        // Truncate some bytes
+        if (dataLength > 2) {
+            dataLength -= 2;
+            entryArgs.setNth(2, dataLength);
+            cm.setABIArguments(entryArgs);
+        }
+
+        const data = try cm.childReadData(dataAddress, dataLength);
+        defer data.deinit();
+        std.debug.print("Got a write on {}: {s}\n", .{ fd, data.items });
+
+        // Handle syscall exit
+        _ = cm.childWaitForSyscall();
+
+        var exitArgs = cm.getABIArguments();
+        dataLength = exitArgs.nth(2);
+        if (dataLength > 2) {
+            // Force the writes to stop after the first one by returning EIO.
+            var result: c_ulonglong = 0;
+            result = result -% c.EIO;
+            exitArgs.setResult(result);
+            cm.setABIArguments(&exitArgs);
+        }
+    }
+};
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -21,121 +184,7 @@ pub fn main() !void {
     } else if (pid == 0) {
         return execChild(&arena, args, pid);
     } else {
-        try interceptChildSyscalls(&arena, pid);
-    }
-}
-
-const cNullPtr: ?*anyopaque = null;
-
-fn execChild(
-    arena: *std.heap.ArenaAllocator,
-    args: []const []const u8,
-    childPid: std.os.pid_t,
-) std.process.ExecvError {
-    _ = c.ptrace(c.PTRACE_TRACEME, childPid, cNullPtr, cNullPtr);
-    return std.process.execv(
-        arena.allocator(),
-        args[1..],
-    );
-}
-
-fn interceptChildSyscalls(
-    arena: *std.heap.ArenaAllocator,
-    pid: std.os.pid_t,
-) !void {
-    var status: i32 = 0;
-
-    while (true) {
-        // Handle syscall entrance
-        _ = c.ptrace(c.PTRACE_SYSCALL, pid, cNullPtr, cNullPtr);
-        _ = c.waitpid(pid, &status, 0);
-        if (status == 0) {
-            break;
-        }
-
-        var regs: c.user_regs_struct = undefined;
-        _ = c.ptrace(c.PTRACE_GETREGS, pid, cNullPtr, &regs);
-
-        var syscall = std.os.linux.syscalls.X64.futex_waitv;
-        if (regs.orig_rax < @intFromEnum(std.os.linux.syscalls.X64.futex_waitv)) {
-            syscall = @enumFromInt(regs.orig_rax);
-        }
-        const fd = regs.rdi;
-        var waitForExit = true;
-        switch (syscall) {
-            .write => {
-                const dataAddress = regs.rsi;
-                var dataLength = regs.rdx;
-
-                // Truncate some bytes
-                if (dataLength > 2) {
-                    regs.rdx -= 2;
-                    dataLength -= 2;
-                    _ = c.ptrace(c.PTRACE_SETREGS, pid, cNullPtr, &regs);
-                }
-
-                var data = std.ArrayList(u8).init(arena.allocator());
-                while (data.items.len < dataLength) {
-                    var word = c.ptrace(
-                        c.PTRACE_PEEKDATA,
-                        pid,
-                        dataAddress + data.items.len,
-                        cNullPtr,
-                    );
-
-                    for (std.mem.asBytes(&word)) |byte| {
-                        if (data.items.len == dataLength) {
-                            break;
-                        }
-                        try data.append(byte);
-                    }
-                }
-
-                std.debug.print("Got a write on {}: {s}\n", .{ fd, data.items });
-            },
-            .close => {
-                std.debug.print("Got a close on {}\n", .{fd});
-            },
-            .fsync => {
-                std.debug.print("Got an fsync on {}\n", .{fd});
-            },
-            else => {
-                waitForExit = false;
-            },
-        }
-
-        if (waitForExit) {
-            // Handle syscall exit
-            _ = c.ptrace(c.PTRACE_SYSCALL, pid, cNullPtr, cNullPtr);
-            _ = c.waitpid(pid, &status, 0);
-            if (status == 0) {
-                break;
-            }
-
-            var regs2: c.user_regs_struct = undefined;
-            _ = c.ptrace(c.PTRACE_GETREGS, pid, cNullPtr, &regs2);
-
-            var syscall2 = std.os.linux.syscalls.X64.futex_waitv;
-            if (regs2.orig_rax < @intFromEnum(std.os.linux.syscalls.X64.futex_waitv)) {
-                syscall2 = @enumFromInt(regs2.orig_rax);
-            }
-            std.debug.assert(syscall == syscall2);
-            switch (syscall2) {
-                .write => {
-                    std.debug.assert(regs.rsi == regs2.rsi);
-                    std.debug.assert(regs.rdx == regs2.rdx);
-
-                    var dataLength = regs.rdx;
-                    if (dataLength > 2) {
-                        // Force the writes to stop after the first one.
-                        // -EPERM == maxValue(c_ulonglong)
-                        // -EIO   == maxValue(c_ulonglong)  -4
-                        regs2.rax = std.math.maxInt(c_ulonglong) - 4;
-                        _ = c.ptrace(c.PTRACE_SETREGS, pid, cNullPtr, &regs2);
-                    }
-                },
-                else => {},
-            }
-        }
+        var cm = ChildManager{ .arena = &arena, .childPid = pid };
+        try cm.childInterceptSyscalls();
     }
 }
